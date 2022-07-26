5f7a6240e8489daa91a8cbb26b2c3eeb13d6a8a7
angel.rodriguez <angel.rodriguez@ironbit.com.mx>
2022-07-26 15:46:21 -0500

    Se crean los componentes de Episodios y Locacion para utilizar Router y se agregar un Navbar en el proyecto.

--- src/App.js
+++ src/App.js
@@ -8,53 +8,78 @@ import Pagination from "./components/Pagination/Pagination";
 import Filter from "./components/Filter/Filter";
 import Navbar from "./components/Navbar/Navbar";
 
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Episodes from "./Pages/Episodes";
+import Location from "./Pages/Location";
+
 function App() {
-  let [pageNumber, updatePageNumber] = useState(1);
-  let [status, updateStatus] = useState("");
-  let [gender, updateGender] = useState("");
-  let [species, updateSpecies] = useState("");
-  let [fetchedData, updateFetchedData] = useState([]);
-  let [search, setSearch] = useState("");
-  let { info, results } = fetchedData;
+    return (
+        <Router>
+            <div className="App">
+                <Navbar />
+            </div>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                {/* <Route path="/:id" element={<CardDetails />} /> */}
 
-  // let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
-  let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+                <Route path="/episodes" element={<Episodes />} />
+                {/* <Route path="/episodes/:id" element={<CardDetails />} /> */}
 
-  useEffect(() => {
-    (async function () {
-      let data = await fetch(api).then((res) => res.json());
-      updateFetchedData(data);
-    })();
-  }, [api]);  
+                <Route path="/location" element={<Location />} />
+                {/* <Route path="/location/:id" element={<CardDetails />} /> */}
+            </Routes>
+        </Router>
+    );
+}
 
-  return (
-    <div className="App">
-      <h1 className="text-center mb-3">Characters</h1>
-      <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
-      <div className="container">
-        <div className="row">
-          <Filter
-            pageNumber={pageNumber}
-            status={status}
-            updateStatus={updateStatus}
-            updateGender={updateGender}
-            updateSpecies={updateSpecies}
-            updatePageNumber={updatePageNumber}
-          />
-          <div className="col-lg-8 col-12">
-          <div className="row">
-            <Card results={results} />
-          </div>
-          </div>
+const Home = () => {
+    let [pageNumber, updatePageNumber] = useState(1);
+    let [status, updateStatus] = useState("");
+    let [gender, updateGender] = useState("");
+    let [species, updateSpecies] = useState("");
+    let [fetchedData, updateFetchedData] = useState([]);
+    let [search, setSearch] = useState("");
+    let { info, results } = fetchedData;
+
+    // let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}`;
+    let api = `https://rickandmortyapi.com/api/character/?page=${pageNumber}&name=${search}&status=${status}&gender=${gender}&species=${species}`;
+
+    useEffect(() => {
+        (async function () {
+            let data = await fetch(api).then((res) => res.json());
+            
+            updateFetchedData(data);
+        })();
+    }, [api]);  
+
+    return (
+        <div className="App">
+            <h1 className="text-center mb-3">Characters</h1>
+            <Search setSearch={setSearch} updatePageNumber={updatePageNumber} />
+            <div className="container">
+                <div className="row">
+                    <Filter
+                        pageNumber={pageNumber}
+                        status={status}
+                        updateStatus={updateStatus}
+                        updateGender={updateGender}
+                        updateSpecies={updateSpecies}
+                        updatePageNumber={updatePageNumber}
+                    />
+                    <div className="col-lg-8 col-12">
+                        <div className="row">
+                            <Card results={results} />
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <Pagination
+                info={info}
+                pageNumber={pageNumber}
+                updatePageNumber={updatePageNumber}
+            />
         </div>
-      </div>
-      <Pagination
-        info={info}
-        pageNumber={pageNumber}
-        updatePageNumber={updatePageNumber}
-      />
-    </div>
-  );
+    );
 }
 
 export default App;
